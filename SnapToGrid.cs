d0dd74a9716725ca87b9fc9cd65ba49cf27fca48
Maxim <91284413+MaximusKilly@users.noreply.github.com>
2022-08-06 17:24:19 +0400

    Add files via upload

---
+++ SnapToGrid.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapToGrid : MonoBehaviour
+{
+    [SerializeField] [Range(1,30)] int Size;
+    void Update()
+    {
+        Vector3 SnapPos;
+        SnapPos.x = Mathf.RoundToInt(transform.position.x / Size) * Size;
+        SnapPos.y = 0;
+        SnapPos.z = Mathf.RoundToInt(transform.position.z / Size) * Size;
+        transform.position = SnapPos;
+    }
+}
